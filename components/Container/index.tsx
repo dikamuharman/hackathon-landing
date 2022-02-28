327936a0ca7f62b2092dc1e779f182dfc786a66e
dikamuharman <dika1254@gmail.com>
2022-02-28 10:14:20 +0700

    add new component container

---
+++ components/Container/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type ContainerProps = {
+  as: 'div' | 'section' | 'main'
+  children: React.ReactNode
+  className?: string
+}
+
+const Container = ({ as = 'div', children, className }: ContainerProps) => {
+  const Component = as || 'div'
+
+  return (
+    <Component
+      className={`container mx-auto px-5 pt-8 md:px-16 lg:px-20 ${className}`}
+    >
+      {children}
+    </Component>
+  )
+}
+
+export default Container
