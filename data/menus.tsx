0c3ccc3ac3c1d6b0e702d799045fb92ddd2fc8f1
dikamuharman <dika1254@gmail.com>
2022-02-28 11:06:20 +0700

    add new menu data

---
+++ data/menus.tsx
@@ -0,0 +1,25 @@
+type Menu = {
+  name: string
+  url: string
+}
+
+const menus: Array<Menu> = [
+  {
+    name: 'About us',
+    url: '/',
+  },
+  {
+    name: 'Timeline',
+    url: '/',
+  },
+  {
+    name: 'Prize',
+    url: '/',
+  },
+  {
+    name: 'FAQ',
+    url: '/',
+  },
+]
+
+export default menus
