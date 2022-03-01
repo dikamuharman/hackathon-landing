cdf17419b1d2d900a15d973d16dda4071e9f15ef
dikamuharman <dika1254@gmail.com>
2022-03-02 05:38:35 +0700

    add subsriben section

---
+++ components/Section/SubscribeSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../Container'
+
+const SubscribeSection = () => {
+  return (
+    <Container as="section" className="-mt-12">
+      <div className="-mt-10 rounded-lg bg-white px-4 py-9 shadow">
+        Subscription
+      </div>
+    </Container>
+  )
+}
+
+export default SubscribeSection
