d1eab8fbd494b7a6b7a3a04e66ebf6de0df532e2
dikamuharman <dika1254@gmail.com>
2022-03-02 12:21:14 +0700

    change image

--- components/Section/AboutSection.tsx
+++ components/Section/AboutSection.tsx
@@ -6,18 +6,15 @@ import Button from '../Buttons'
 
 const AboutSection = () => {
   return (
-    <Container
-      as="section"
-      className="mb-20 flex flex-col lg:flex-row lg:gap-x-14"
-    >
-      <div className="flex justify-center lg:w-1/2">
-        <Image src={AboutImage} alt="About image" />
+    <Container as="section" className="mb-20 flex flex-col lg:flex-row">
+      <div className="flex justify-start lg:w-1/2">
+        <Image src={AboutImage} alt="About image" width={500} height={466} />
       </div>
       <div className="mt-8 text-center lg:w-1/2 lg:text-left">
         <h2 className="mb-7 text-3xl font-bold text-textPrimary lg:mb-10 lg:text-5xl">
           About Us
         </h2>
-        <p className="mb-7 text-sm leading-6 lg:mb-10 lg:text-base lg:leading-7">
+        <p className="mb-7 text-sm leading-6 lg:mb-10 lg:text-lg lg:leading-9">
           HackFest Indonesia is Indonesia’s hackathon <br />
           held by GDSC Indonesia where university students <br />
           of different skills from all over Indonesia can come <br />
